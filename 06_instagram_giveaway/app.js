af4f59d111dd82610c7996b12a9587e0ea44fc6d
Aleksandr <ledich19@gmail.com>
2023-10-25 16:08:06 +0300

    feat: add some workers

--- 06_instagram_giveaway/app.js
+++ 06_instagram_giveaway/app.js
@@ -1,11 +1,33 @@
 import { countUniqueValues, countInAllFiles, countInAtleastTen } from './modules/countFunctions.js';
 import executionTimeAsyncFn from './modules/executionTimeAsyncFn.js';
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
 
 const uniqueValues = executionTimeAsyncFn(countUniqueValues);
 const existInAllFiles = executionTimeAsyncFn(countInAllFiles);
 const existInAtleastTen = executionTimeAsyncFn(countInAtleastTen);
 
 const folderPath = './data';
-uniqueValues(folderPath);
-existInAllFiles(folderPath);
-existInAtleastTen(folderPath);
+
+if (isMainThread) {
+  new Worker(new URL(import.meta.url), {
+    workerData: { func: 'uniqueValues', folderPath },
+  });
+
+  new Worker(new URL(import.meta.url), {
+    workerData: { func: 'existInAllFiles', folderPath },
+  });
+
+  new Worker(new URL(import.meta.url), {
+    workerData: { func: 'existInAtleastTen', folderPath },
+  });
+} else {
+  const { func, folderPath } = workerData;
+
+  if (func === 'uniqueValues') {
+    uniqueValues(folderPath);
+  } else if (func === 'existInAllFiles') {
+    existInAllFiles(folderPath);
+  } else if (func === 'existInAtleastTen') {
+    existInAtleastTen(folderPath);
+  }
+}
