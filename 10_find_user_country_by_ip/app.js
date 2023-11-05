351961793a08eb8981646db9c57d88dcec199af9
Aleksandr <ledich19@gmail.com>
2023-11-05 23:16:01 +0200

    refactor: clean 10

--- 10_find_user_country_by_ip/app.js
+++ 10_find_user_country_by_ip/app.js
@@ -1,21 +1,13 @@
 const express = require('express');
-const authRouter = require('./controllers/auth.controller');
-const { errorHandler, tokenExtractor } = require('./utils/middleware');
+const { errorHandler, unknownEndpoint } = require('./utils/middleware');
 const userRouter = require('./controllers/user.controller');
 const app = express();
 
 app.use(express.json());
 
-app.use(tokenExtractor);
-app.use('/', authRouter);
 app.use('/user', userRouter);
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({
-    success: false,
-    error: 'unknown endpoint',
-  });
-};
+
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
--- 10_find_user_country_by_ip/utils/healper.js
+++ 10_find_user_country_by_ip/utils/healper.js
@@ -1,16 +1,4 @@
-const { sign } = require('jsonwebtoken');
 const { readFile } = require('fs/promises');
-
-const createAccessToken = async (payload) => {
-  const jwtToken = await sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_TIME,
-  });
-  return jwtToken;
-};
-const createRefreshToken = async (payload) => {
-  const refreshToken = await sign(payload, process.env.JWT_REFRESH_SECRET);
-  return refreshToken;
-};
 
 const readCVSFile = async (filePath) => {
   try {
@@ -36,8 +24,6 @@ const memoize = (func) => {
 };
 
 module.exports = {
-  createAccessToken,
-  createRefreshToken,
   readCVSFile,
   memoize,
 };
